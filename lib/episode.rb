c6d0d5edc63af5b29b29f9caed80d00294bfc654
Jerod Santo <jerod.santo@gmail.com>
2015-08-02 15:39:21 -0500

    add KRW twitter handles, move `image_path` to episode method call

--- lib/episode.rb
+++ lib/episode.rb
@@ -1,6 +1,10 @@
 require_relative "film"
 
 class Episode < Film
+  def image_path
+    "/images/season-#{@season.number}/#{slug}.jpg"
+  end
+
   def slug
     @data.slug || @data.twitter || @data.title.downcase.gsub(" ", "-")
   end
