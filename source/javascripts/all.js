744f8ce96d3240ecbf2b722c4223876c0075bdf9
Jerod Santo <jerod.santo@gmail.com>
2015-08-05 15:47:21 -0500

    switch single episode page to JS-based iframe player

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,42 @@
 //= require vanilla-modal
 
 (function() {
-  var modal = new VanillaModal();
+  var body = document.getElementsByTagName("body")[0];
+  var template = body.getAttribute("data-template");
+
+  if (template === "season") {
+    var modal = new VanillaModal();
+  }
+
+  if (template === "single") {
+    var playerTag = document.getElementById("player");
+    var tag = document.createElement("script");
+    tag.src = "https://www.youtube.com/iframe_api";
+
+    var firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    var player;
+    window.onYouTubeIframeAPIReady = function() {
+      player = new YT.Player("player", {
+        height: "560",
+        width: "315",
+        videoId: playerTag.getAttribute("data-id"),
+        playerVars: {
+          showinfo: 0,
+          autoplay: 1
+        },
+        events: {
+          "onReady": onPlayerReady,
+          "onStateChange": onPlayerStateChange
+        }
+      });
+    };
+
+    function onPlayerReady(event) {
+    }
+
+    function onPlayerStateChange(event) {
+    }
+  }
 })();
