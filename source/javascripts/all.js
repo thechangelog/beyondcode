82d97b0b521ec3b9cb9577ec52502cb0f5b8eef8
Jerod Santo <jerod.santo@gmail.com>
2015-08-08 13:56:59 -0500

    give JS an object container

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,28 +1,36 @@
 //= require vanilla-modal
 //= require layzrjs/dist/layzr.min.js
 
-(function() {
-  var layzr = new Layzr({
-    container: null,
-    selector: "[data-src]",
-    attr: "data-src",
-    retinaAttr: "data-src-retina",
-    bgAttr: "data-src-bg",
-    hiddenAttr: "data-src-hidden",
-    threshold: 0,
-    callback: function () {
-      this.classList.add("is-loaded");
-    }
-  });
+window.BC = {
+  init: function() {
+    var body = document.getElementsByTagName("body")[0];
+    var template = body.getAttribute("data-template");
 
-  var body = document.getElementsByTagName("body")[0];
-  var template = body.getAttribute("data-template");
+    var layzr = new Layzr({
+      container: null,
+      selector: "[data-src]",
+      attr: "data-src",
+      retinaAttr: "data-src-retina",
+      bgAttr: "data-src-bg",
+      hiddenAttr: "data-src-hidden",
+      threshold: 0,
+      callback: function () {
+        this.classList.add("is-loaded");
+      }
+    });
 
-  if (template === "season") {
-    var modal = new VanillaModal();
-  }
+    this[template]();
+  },
 
-  if (template === "single") {
+  index: function() {
+    // nothing yet
+  },
+
+  season: function() {
+    this.modal = new VanillaModal();
+  },
+
+  single: function() {
     var playerTag = document.getElementById("player");
     var tag = document.createElement("script");
     tag.src = "https://www.youtube.com/iframe_api";
@@ -53,4 +61,6 @@
     function onPlayerStateChange(event) {
     }
   }
-})();
+};
+
+BC.init();
